b73ea7c948beba1c2bfe6c29f388c621d04d2ced
NithishKumarKL <95710496+NithishKumarKL@users.noreply.github.com>
2023-04-11 20:18:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res)=>{
-    res.end('Hello from node\n');
+    res.end('Hello from JavaSript\n');
 });
 server.listen(4242, () =>{
     console.log('Server is running...');
